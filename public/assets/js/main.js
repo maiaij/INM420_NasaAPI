474cf44b6a0d74e802f5a31e008b3740d268c604
Maia Johnson <maia.johnson4@gmail.com>
2025-02-10 13:18:33 -0500

    fonts added; comments added; assignment complete

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,7 @@
-//console.log("wee woo");
-
-// IF REQUEST IS EMPTY SAY PHOTO UNAVAILABLE
+/** 
+ *  Maia Johnson
+ *  February 10, 2025
+*/
 
 // set the current day string for the default display
 var today = new Date();
@@ -8,32 +9,28 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
-console.log(today);
-console.log(dd);
-console.log(mm);
-console.log(yyyy);
-
-// GET RID OF THE -1
+// format the date for the request
 var default_date = `${yyyy}-${mm}-${dd}`;
 var selected_date = default_date;
-console.log(selected_date);
+
+//set the max date as today
+document.getElementById("date_picker").max = default_date;
 
 var request_string = `https://api.nasa.gov/planetary/apod?api_key=scZ5BKkgDMqOfUgjoZbu1nXEFkG8uuDCCdW9PKK5&start_date=${selected_date}&end_date=${selected_date}`;
-
 var test_string = "https://api.nasa.gov/planetary/apod?api_key=scZ5BKkgDMqOfUgjoZbu1nXEFkG8uuDCCdW9PKK5&start_date=2017-07-08&end_date=2017-07-10";
 
-//document.getElementById("p2").style.color = "blue";
-//background-image: url();
 
 // get the API data
 async function getData(api_url){
     try {
+        // make a request to the api
         const response = await fetch(api_url);
-        //console.log(response);
         
+        // convert the response to a json
         const data = await response.json();
-        console.log(data[0]);
+        //console.log(data[0]);
 
+        //pull the information you want from the request
         //select html where the image & description will go
         const page_img = document.getElementById("pictureOTD");
         const img_desc = document.getElementById("img_desc");
@@ -45,7 +42,7 @@ async function getData(api_url){
 
         // put the date in a Date object to convert to text
         var tmp_date = new Date(`${data[0].date}T00:00:00`);
-        console.log(tmp_date.toDateString());
+        //console.log(tmp_date.toDateString());
 
         //add the text items
         img_desc.innerHTML = data[0].explanation;
@@ -64,10 +61,6 @@ getData(request_string);
 document.getElementById("date_picker").addEventListener("change", function() {
     var input = this.value;
     selected_date = input;
-    console.log(input); //e.g. 2015-11-13
     request_string = `https://api.nasa.gov/planetary/apod?api_key=scZ5BKkgDMqOfUgjoZbu1nXEFkG8uuDCCdW9PKK5&start_date=${selected_date}&end_date=${selected_date}`;
     getData(request_string);
 });
-
-
-//getData(test_string);
